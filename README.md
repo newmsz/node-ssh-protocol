e7d7b178d3773ec417213d62069554b6e01343e4
Sungsoo Moon <newms@durooh.com>
2013-04-04 14:18:37 +0900

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ node-ssh-protocol
 =================
 
 Nodejs implementation of ssh protocol
+
+Simple Usage
+```
+var fs = require('fs');
+var ssh_protocol = require('ssh-protocol'),
+	ssh_server = require('net').createServer().listen(22);
+
+ssh_protocol.config({ 
+	softwareversion: 'version_0.0',
+	'ssh-rsa': { 
+		publickey: fs.readFileSync('_t_rsa_b_2048.pub').toString(),
+		privatekey: fs.readFileSync('_t_rsa_b_2048.key').toString() 
+	},
+	'banner': {
+		message: 'banner messages...\r\n'
+	}
+});
+
+ssh_protocol.on('userauth', function (request, response) {
+	response.success('sole_user');
+});
+
+ssh_protocol.on('data', function (request, response) {
+	response.stdout(request.data);
+});
+
+ssh_protocol.listen(ssh_server);
+```
---
+++ ssh-protocol.js
@@ -0,0 +1,35 @@
+var	_ = require('underscore');
+var SSH = require('./lib/ssh');
+
+var config = { 
+	softwareversion: 'nodesshprotocol_0.0.1', 
+	comments: '',
+	'ssh-rsa': null 
+};
+
+var self = module.exports;
+
+_.extend(self, new (require('events').EventEmitter)());
+
+exports.config = function(conf) {
+	config = _.defaults(conf, config);
+	if(config['ssh-rsa']) {
+		if(!config['ssh-rsa'].publickey) throw new Error('no publickey found in "ssh-rsa" configuration');
+		if(!config['ssh-rsa'].privatekey) throw new Error('no privatekey found in "ssh-rsa" configuration');
+		SSH.enableSSHRSA(config['ssh-rsa'].publickey, config['ssh-rsa'].privatekey); 
+	}
+	if(config['banner']) {
+		SSH.setBanner(config['banner'].message, config['banner'].language);
+	}
+};
+
+exports.listen = function (soc, cb) {
+	soc.on('connection', _onconnection);
+};
+
+function _onconnection(conn) {
+	var ssh = new (SSH.SSH)(conn);
+	ssh.hello(config.softwareversion, config.comments);
+	ssh.on('userauth', function (request, response) { self.emit('userauth', request, response); });
+	ssh.on('data', function (request, response) { self.emit('data', request, response); });
+}
